9afdb00c8975d4db2ab4fcc600e54b839e2e2b9c
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-04-01 23:34:19 +0400

    first commit

---
+++ kod _time.js
@@ -0,0 +1,90 @@
+
+// TASK 1
+// let numbers = [1, 2, 3, 44, 67, 6, 8];
+// const numbers_max = Math.max(...numbers);
+// console.log(numbers_max);
+
+// let a = [1,2,3]
+// let b = [...a, "salam","demo",9]
+// console.log(b);
+
+
+
+// TASK 2
+
+// 3,5,7,11
+//             true false true
+// let numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 21];
+// const numbers_prime = (num) => {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+// 
+// const cavab = numbers.filter((num) => numbers_prime(num));
+
+// console.log(cavab);
+
+
+
+// TASK 3
+// let data = ["salam","backend","dunyali","frontend"]
+// const data_filter = data.filter((word) => word.length === 7)
+// console.log(data_filter);
+
+
+// TASK 4
+
+// let boy = +prompt("Boy daxil edin: ");
+// let kilo = +prompt("Kilo daxil edin: ");
+
+
+// let bki = kilo / boy ** 2;
+// if (18 >= bki && bki >= 0) {
+//   alert("Ideal cekiden asagi");
+// } else if (25 > bki && bki >= 18) {
+//   alert("Ideal");
+// } else if (30 > bki && bki >= 25) {
+//   alert("Artiq ceki");
+// } else if (40 > bki && bki >= 30) {
+//   alert("Obez");
+// } else if (40 < bki) {
+//   alert("Morbid derecede artiq ceki");
+// } else {
+//   alert("Xais olunur dogru deyerleri daxil edin...");
+// }
+
+
+// TASK 5
+// let point = +prompt("Point: ");
+// if (100 >= point && point >= 90) {
+//   alert("Ela");
+// } else if (90 > point && point >= 75) {
+//   alert("Yaxsi");
+// } else if (75 > point && point >= 55) {
+//   alert("Orta");
+// } else if (55 > point && point >= 35) {
+//   alert("Kafi");
+// } else if (35 > point && point >= 0) {
+//   alert("Pis");
+// } else {
+//   alert("Xais olunur 0-100 arasi reqem daxil edin...");
+// 
+
+
+
+
+// TASK 6
+// let a = +prompt("Birinci teref: ");
+// let b = +prompt("Ikinci teref: ");
+// const hipetonuz = (a, b) => Math.sqrt(a ** + b **);
+// console.log(hipetonuz(a, b));
+
+
+
+
+
+
+
+
+
+
+
